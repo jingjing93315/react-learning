235d2a3acb3d291dcec58d57f5a96935ad1d67c3
gaojing <gaojing@tyb365.com>
2022-01-12 14:24:32 +0800

    react配置代理

---
+++ react_staging/hello-react/src/setupProxy.js
@@ -0,0 +1,25 @@
+// CommonJS 语法写法
+
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = function (app){
+  // api 遇到api前缀的请求，就会触发该代理设置
+  app.use('/api', createProxyMiddleware(
+    {
+      target:'http://localhost:5000', // 请求转发给谁
+      changeOrigin: true, // 控制服务器收到的请求头中Host的值
+      pathRewrite: {
+        '^/api': '' // 重写请求路径
+      }
+    }
+  ))
+  app.use('/api1', createProxyMiddleware(
+    {
+      target:'http://localhost:5001',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api1': ''
+      }
+    }
+  ))
+}
