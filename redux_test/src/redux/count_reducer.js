bc54afb36f7041b1052262b893d99b66a73bed08
gaojing <gaojing@tyb365.com>
2022-03-23 10:51:31 +0800

    redux精简版Count

---
+++ redux_test/src/redux/count_reducer.js
@@ -0,0 +1,19 @@
+/**
+ * 1.该文件用于创建于一个为Count组件服务的reducer，reducer的本质
+ * 是一个函数
+ * 2. reducer函数会接到两个参数：之前的状态(preState)和动作对象(action)
+ */
+const initState = 0 // 形参的默认值,初始化状态
+export default function countReducer(preState=initState,action){
+  // 从action对象中获取type，data
+  const { type,data } = action
+  // 根据type决定如何加工数据
+  switch (type) {
+    case 'increment':
+     return preState + data
+    case 'decrement':
+     return preState - data
+    default: // 初始化
+      return preState
+  }
+}
